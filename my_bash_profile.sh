5dc06713116ce44e0ea9811cf8ab71fa2046239a
Joel <joel.lim@circles.asia>
2021-02-05 14:06:26 +0800

    githome

--- my_bash_profile.sh
+++ my_bash_profile.sh
@@ -43,7 +43,7 @@ monrb() {
 }
 
 # git
-alias githome="cd $(git rev-parse --show-toplevel)"
+alias githome="cd $(git rev-parse --show-cdup)"
 alias checkout="git checkout"
 alias push="git push"
 alias pull="git pull"
