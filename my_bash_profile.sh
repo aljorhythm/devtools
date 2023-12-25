5e86cc65b57b95e5308d6dd918b3e4de6804d020
Joel Lim <103879u@gmail.com>
2023-12-26 00:20:56 +0800

    edit

--- my_bash_profile.sh
+++ my_bash_profile.sh
@@ -3,8 +3,8 @@ echo running my_bash_profile
 alias o='open .'
 alias e=exit
 alias dls='cd ~/Downloads'
-alias kc=kubectl
-alias k=kubectl
+# alias kc=kubectl
+# alias k=kubectl
 alias t="open -a /System/Applications/Utilities/Terminal.app ."
 alias it="open -a /Applications/iTerm.app ."
 alias mk="make"
